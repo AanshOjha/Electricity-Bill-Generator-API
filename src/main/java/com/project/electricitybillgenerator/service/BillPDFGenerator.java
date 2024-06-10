57aaf3112dea39bf85ed9d88ed2313067e52f99b
AanshOjha <1aanshojha@gmail.com>
2024-06-10 18:20:12 +0530

    Fixed typo in PDF

--- src/main/java/com/project/electricitybillgenerator/service/BillPDFGenerator.java
+++ src/main/java/com/project/electricitybillgenerator/service/BillPDFGenerator.java
@@ -101,8 +101,8 @@ public class BillPDFGenerator {
                         Unit Price: %.2f
                         Bill: %.2f
                         18%% GST: %.2f
-                        """, date,
-                    currentReading, previousReading, unitsConsumed, unitsConsumed,
+                        """,
+                    date, currentReading, previousReading, unitsConsumed, unitRate,
                     billWithoutGST, gst);
 
             String p4 = String.format("Payable Amount: %.2f",
